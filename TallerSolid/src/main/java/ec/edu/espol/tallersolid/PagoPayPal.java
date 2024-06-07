b6d5fe52682e6d4b9802cdd87e075c19c7076ca6
Alejandro Sornoza <alesvera@espol.edu.ec>
2024-06-06 20:19:05 -0500

    CORRECCION DE VIOLACIONES LISKOV Y SINGLE RESPONSIBILITY

--- TallerSolid/src/main/java/ec/edu/espol/tallersolid/PagoPayPal.java
+++ TallerSolid/src/main/java/ec/edu/espol/tallersolid/PagoPayPal.java
@@ -8,13 +8,16 @@ package ec.edu.espol.tallersolid;
  *
  * @author aleja
  */
+//EN ESTA CLASE SE VIOLA EL PRINCIPIO DE LISKOV YA QUE EN EL METODO REALIZAR COBRO SI EL USUARIO NO ESTA LOGGEADO
+//EL METODO NO HACE NADA, CONTRARIO A LO ESTABLECIDO EN LA CLASE PADRE.
 public class PagoPayPal extends Pago{
     private boolean loggedIn; //conexión a cuenta PayPal
 
     @Override
     public void realizarCobro(double monto){
-        if (!loggedIn)
-            return;
+        if (!loggedIn){
+            //METODO RESPECTIVO PARA HACER LOG IN, LUEGO DE ESO EL METODO SE HABILITA Y SE COMPORTA IGUAL QUE EL PADRE
+        }
         //cargar el monto de compra al medio de pago
     }
 }
